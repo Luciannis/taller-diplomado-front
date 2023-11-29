b8e18e98846bdefe6cdb34675ddc8d7f344b0014
Luciannis <fcoesteban13@gmail.com>
2023-11-28 21:28:23 -0300

    se muestran los valores de los bitcoins y saldo clp que tiene el usuario mas el saldo total

--- src/components/Transactions.js
+++ src/components/Transactions.js
@@ -3,29 +3,50 @@ import axios from 'axios';
 
 const Transactions = () => {
     const URL = "http://localhost:8000";
-    const [valorCLP, setValorCLP] = useState("")
-    const [valorBitcoin, setValorBitcoin] = useState("")
     const [saldoTotal, setsaldoTotal] = useState("")
     const [userCLP, setUserCLP] = useState("")
     const [userBitcoin, setUserBitcoin] = useState("")
+    const [valorBitcoin,setValorBitcoin] = useState("")
+
+    const formaterBTC = (value) => {
+        return parseFloat(value).toFixed(0);
+      };
+
+    const formaterPrecio = (value) => {
+        const formatter = new Intl.NumberFormat('es-CL', {
+          style: 'currency',
+          currency: 'CLP',
+        });
+        return formatter.format(value);
+      };
+
     const getPrecios = async() => {
         try {
           const {data} = await axios.get(`${URL}/price`)
-            console.log("data");
-            console.log(data.bitcoin.clp);
             if(data){
-                setValorCLP(data.bitcoin.clp)
+                setValorBitcoin(data.bitcoin.clp)
             }
         } catch (error) {
           console.log("Error al obtener los precios", error);
         }
-      }
-      const calcularSaldoTotal = ()=>  {
-        saldoTotal = (userBitcoin*valorBitcoin) + (userCLP)
-        setsaldoTotal(saldoTotal);
-      }
+    }
+    //   const calcularSaldoTotal = ()=>  {
+    //     saldoTotal = (userBitcoin*valorBitcoin) + (userCLP)
+    //     setsaldoTotal(saldoTotal);
+    //   }
       useEffect(() => {
+        async function saldoUser(){
+            const token = localStorage.getItem('token')
+            if(token){
+                const {data} = await axios.get(`${URL}/wallet`,{headers: {'Authorization': `Bearer ${token}`} })
+                setUserBitcoin(data.balance_btc)
+                setUserCLP(data.balance_clp)
+                console.log("saldo del pana");
+                console.log(data);
+            }
+        }
         getPrecios()
+        saldoUser()
       },[])
 
     const labelStyle = {
@@ -39,10 +60,16 @@ const Transactions = () => {
                 <div className="card-body">
                 <h2 className="card-title text-center mb-4">Billetera</h2>
                 <div className="mb-3 d-flex justify-content-start">
-                    <label className="form-label" style={labelStyle}>Tienes 5 Bitcoins</label>
+                    <label className="form-label" style={labelStyle}>1 BTC = {formaterPrecio(parseFloat(valorBitcoin))} CLP</label>
                 </div>
                 <div className="mb-3 d-flex justify-content-start">
-                    <label className="form-label">Tienes {valorCLP} CLP</label>
+                    <label className="form-label" style={labelStyle}>Tienes <b> {formaterBTC(userBitcoin)} Bitcoins</b></label>
+                </div>
+                <div className="mb-3 d-flex justify-content-start">
+                    <label className="form-label">Tienes<b> {formaterPrecio(parseFloat(userCLP))} CLP </b></label>
+                </div>
+                <div className="mb-3 d-flex justify-content-start">
+                    <label className="form-label">Saldo Total <b>{formaterPrecio(parseFloat(userCLP) + userBitcoin*valorBitcoin)} pesos</b></label>
                 </div>
                 <div className="mb-3 d-flex justify-content-start">
                     <label className="form-label">Comprar</label>
@@ -54,7 +81,7 @@ const Transactions = () => {
                     <label className="form-label">Vender</label>
                     <input></input>
                     <label>BTC</label>
-                    <button className="btn btn-danger">Comprar</button>
+                    <button className="btn btn-danger">Vender</button>
                 </div>
                 </div>
             </div>
